c6c7b2145505abbe7fb0e4b09df07c326030a7a3
wilbobraggins <wilbobraggins@gmail.com>
2018-09-10 20:31:28 +0000

    static pages

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -9,16 +9,15 @@ class Instructor::SectionsController < ApplicationController
     @section = current_course.sections.create(section_params)
     redirect_to instructor_course_path(current_course)
   end
-
+ 
   def update
-    current_lesson.update_attributes(section_params)
+    current_section.update_attributes(section_params)
   end
 
   private
 
   def current_section
-    @current_section ||= Lesson.find(params[:id])
-    render plain: 'updated!'
+    @current_section ||= Section.find(params[:id])
   end
 
   def require_authorized_for_current_section
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,14 @@ class StaticPagesController < ApplicationController
   
   def index
   end
+
+  def privacy
+  end
+
+  def team
+  end
+
+  def career
+  end
   
 end

31bfde190d89afaebe2b849c7f676fc05107d431
wilbobraggins <wilbobraggins@gmail.com>
2019-02-17 20:06:05 +0000

    security vunerability

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,13 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'william.marchek@gmail.com',
-  :password => 'topsnuff6',
+  :user_name => ENV["SENDGRID_USERNAME"],
+  :password => ENV["SENDGRID_PASSWORD"],
   :domain => 'https://flixter-will-marchek.herokuapp.com',
   :address => 'smtp.sendgrid.net',
-  :port => 587,
+  :port => "25",
   :authentication => :plain,
   :enable_starttls_auto => true
-}
+  }

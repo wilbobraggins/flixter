5938006c93f0f3a86917919dfc85408c7e8821e9
wilbobraggins <wilbobraggins@gmail.com>
2019-03-07 03:31:11 +0000

    skip_jwt = true in inizilizer/devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   
-  config.omniauth :google_oauth2, 'GOOGLE_CLIENT_ID', 'GOOGLE_CLIENT_SECRET'
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.

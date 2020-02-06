29f20a90eeeff42eb413d6adb5ed95148127a385
William Marchek <wilbobraggins@gmail.com>
2020-02-06 00:07:08 +0000

    fixed heroku precompile

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Flixter
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.assets.initialize_on_precompile = false
     # Add the fonts path
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
